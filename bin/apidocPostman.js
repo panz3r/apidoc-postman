2401e771f2b8514753454f087e62c1bba05998d0
Mattia Panzeri <mattia.panzeri93@gmail.com>
2018-10-08 23:38:38 +0200

    feat(Commander): Migrate 'nomnom' to 'commander-js'

--- bin/apidocPostman.js
+++ bin/apidocPostman.js
@@ -11,70 +11,55 @@
  * Licensed under the MIT license.
  */
 
+var commander = require("commander");
 var path = require("path");
-var nomnom = require("nomnom");
+
 var apidocPostman = require("../lib/index");
 
-var argv = nomnom
-  .option("file-filters", {
-    abbr: "f",
-    default:
-      ".*\\.(clj|cls|coffee|cpp|cs|dart|erl|exs?|go|groovy|ino?|java|js|jsx|kt|litcoffee|lua|p|php?|pl|pm|py|rb|scala|ts|vue)$",
-    list: true,
-    help: "RegEx-Filter to select files that should be parsed (multiple -f can be used)."
-  })
+function collect(v, a) {
+  a.push(v);
+  return a;
+}
 
-  .option("exclude-filters", {
-    abbr: "e",
-    default: "",
-    list: true,
-    help: "RegEx-Filter to select files / dirs that should not be parsed (many -e can be used)."
-  })
+var program = commander
+  .option("-f --file-filters <file-filters>", "RegEx-Filter to select files that should be parsed (multiple -f can be used).", collect, [])
 
-  .option("input", { abbr: "i", default: "./", list: true, help: "Input / source dirname." })
+  .option("-e, --exclude-filters <exclude-filters>", "RegEx-Filter to select files / dirs that should not be parsed (many -e can be used).", collect, [])
 
-  .option("output", { abbr: "o", default: "./doc/", help: "Output dirname." })
+  .option("-i, --input <input>",  "Input/source dirname.", collect, [])
 
-  .option("template", {
-    abbr: "t",
-    default: path.join(__dirname, "../template/"),
-    help: "Use template for output files."
-  })
+  .option("-o, --output <output>", "Output dirname.", "./doc/")
 
-  .option("config", { abbr: "c", default: "./", help: "Path to directory containing config file (apidoc.json)" })
+  .option("-t, --template <template>", "Use template for output files.", path.join(__dirname, "../template/"))
 
-  .option("private", { abbr: "p", default: false, help: "Include private APIs in output." })
+  .option("-c, --config <config>", "Path to directory containing config file (apidoc.json).", "./")
 
-  .option("verbose", { abbr: "v", flag: true, default: false, help: "Verbose debug output." })
+  .option("-p, --private", "Include private APIs in output.", false)
 
-  .option("help", { abbr: "h", flag: true, help: "Show this help information." })
+  .option("-v, --verbose", "Verbose debug output.", false)
 
-  .option("debug", { flag: true, default: false, help: "Show debug messages." })
+  .option("-d, --debug", "Show debug messages.", false)
 
-  .option("color", { flag: true, default: true, help: "Turn off log color." })
+  .option("-c, --color", "Turn off log color.", true)
 
-  .option("parse", {
-    flag: true,
-    default: false,
-    help: "Parse only the files and return the data, no file creation."
-  })
+  .option("--parse", "Parse only the files and return the data, no file creation.", false)
 
-  .option("parse-filters", { list: true, help: "Optional user defined filters. Format name=filename" })
-  .option("parse-languages", { list: true, help: "Optional user defined languages. Format name=filename" })
-  .option("parse-parsers", { list: true, help: "Optional user defined parsers. Format name=filename" })
-  .option("parse-workers", { list: true, help: "Optional user defined workers. Format name=filename" })
+  .option("--parse-filters <parse-filters>", "Optional user defined filters. Format name=filename", collect, [])
+  .option("--parse-languages <parse-languages>", "Optional user defined languages. Format name=filename", collect, [])
+  .option("--parse-parsers <parse-parsers>", "Optional user defined parsers. Format name=filename", collect, [])
+  .option("--parse-workers <parse-workers>", "Optional user defined workers. Format name=filename", collect, [])
 
-  .option("silent", { flag: true, default: false, help: "Turn all output off." })
+  .option("-s, --silent", "Turn all output off.", false)
 
-  .option("simulate", { flag: true, default: false, help: "Execute but not write any file." })
+  .option("--simulate", "Execute but not write any file.", false)
 
-  .option("markdown", { default: true, help: "Turn off default markdown parser or set a file to a custom parser." })
+  .option("-m, --markdown", "Turn off default markdown parser or set a file to a custom parser.", true)
 
-  .option("line-ending", { help: "Turn off autodetect line-ending. Allowed values: LF, CR, CRLF." })
+  .option("-l, --line-ending <line-ending>", "Turn off autodetect line-ending. Allowed values: LF, CR, CRLF.")
 
-  .option("encoding", { default: "utf8", help: "Set the encoding of the source code. [utf8]." })
+  .option("--encoding <encoding>", "Set the encoding of the source code. [utf8].", "utf8")
 
-  .parse();
+  .parse(process.argv);
 
 /**
  * Transform parameters to object
@@ -99,26 +84,26 @@ function transformToObject(filters) {
 }
 
 var options = {
-  excludeFilters: argv["exclude-filters"],
-  includeFilters: argv["file-filters"],
-  src: argv["input"],
-  dest: argv["output"],
-  template: argv["template"],
-  config: argv["config"],
-  apiprivate: argv["private"],
-  verbose: argv["verbose"],
-  debug: argv["debug"],
-  parse: argv["parse"],
-  colorize: argv["color"],
-  filters: transformToObject(argv["parse-filters"]),
-  languages: transformToObject(argv["parse-languages"]),
-  parsers: transformToObject(argv["parse-parsers"]),
-  workers: transformToObject(argv["parse-workers"]),
-  silent: argv["silent"],
-  simulate: argv["simulate"],
-  markdown: argv["markdown"],
-  lineEnding: argv["line-ending"],
-  encoding: argv["encoding"]
+  includeFilters: program.fileFilters.length ? program.fileFilters : [".*\\.(clj|cls|coffee|cpp|cs|dart|erl|exs?|go|groovy|ino?|java|js|jsx|kt|litcoffee|lua|p|php?|pl|pm|py|rb|scala|ts|vue)$"],
+  excludeFilters: program.excludeFilters.length ? program.excludeFilters : [""],
+  src: program.input.length ? program.input : ["./"],
+  dest: program.output,
+  template: program.template,
+  config: program.config,
+  apiprivate: program.private,
+  verbose: program.verbose,
+  debug: program.debug,
+  parse: program.parse,
+  colorize: program.color,
+  filters: transformToObject(program.parseFilters),
+  languages: transformToObject(program.parseLanguages),
+  parsers: transformToObject(program.parseParsers),
+  workers: transformToObject(program.parseWorkers),
+  silent: program.silent,
+  simulate: program.simulate,
+  markdown: program.markdown,
+  lineEnding: program.lineEnding,
+  encoding: program.encoding
 };
 
 if (apidocPostman.createCollection(options) === false) {
